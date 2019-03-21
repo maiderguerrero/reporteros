2f2726188568985e7caaa36a63b44c77330bb3af
maiderguerrero <47318536+maiderguerrero@users.noreply.github.com>
2019-03-21 17:03:20 +0100

    Update style.js

--- style.js
+++ style.js
@@ -17,6 +17,7 @@
 				{ name: 'Título 2', element: 'h4', attributes: { 'class': 'bck-title bck-title2'} },
 				{ name: 'Título 3', element: 'h4', attributes: { 'class': 'bck-title bck-title3'} },
 				{ name: 'Título 4', element: 'h4', attributes: { 'class': 'bck-title bck-title4'} },
+				{ name: 'Título 5', element: 'h4', attributes: { 'class': 'bck-title bck-title5'} },
 
 				{ name: 'Lista ordenada 1', element: 'ol', attributes: { 'class': 'bck-ol bck-ol1' } },
 				{ name: 'Lista ordenada 2', element: 'ol', attributes: { 'class': 'bck-ol bck-ol2' } },
